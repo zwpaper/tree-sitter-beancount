7b335c15f8417cdd98bbc3baac42e58f97496d1a
kWeiZh <kweizh@gmail.com>
2022-01-23 21:15:55 +0800

    feat(grammar): :sparkles: add basic directives parsing
    Signed-off-by: kWeiZh <kweizh@gmail.com>

--- grammar.js
+++ grammar.js
@@ -18,6 +18,9 @@ module.exports = grammar({
 
         entry_type_definition: $ => choice(
             $.open_definition,
+            $.close_definition,
+            $.commodity_definition,
+            $.transaction_definition,
             $.note_definition,
             $.balance_definition,
         ),
@@ -25,6 +28,46 @@ module.exports = grammar({
         open_definition: $ => seq(
             'open',
             $.account,
+            optional(
+                seq(
+                    $.commodity,
+                    repeat(
+                        seq(
+                            ",",
+                            $.commodity,
+                        )
+                    ),
+                )
+            ),
+        ),
+
+        close_definition: $ => seq(
+            'close',
+            $.account,
+        ),
+
+        commodity_definition: $ => seq(
+            'commodity',
+            $.commodity,
+        ),
+
+        transaction_definition: $ => seq(
+            choice(
+                "*",  // Completed transaction, known amounts.
+                "!", // Incomplete transaction, needs confirmation or revision
+            ),
+            $.note,
+            repeat(
+                seq(
+                    $.account,
+                    optional(
+                        seq(
+                            $.amount,
+                            $.commodity,
+                        ),
+                    ),
+                )
+            )
         ),
 
         note_definition: $ => seq(
@@ -37,18 +80,39 @@ module.exports = grammar({
             'balance',
             $.account,
             $.amount,
-            $.currency
+            $.commodity
         ),
 
         comment_definition: $ => seq(
             ';',
+            /.*/,
         ),
 
-        account: $ => /[a-zA-Z][a-zA-Z:]*/,
+        account: $ => seq(
+            choice(
+                "Assets",
+                "Liabilities",
+                "Equity",
+                "Income",
+                "Expenses"
+            ),
+            repeat(
+                seq(
+                    ":",
+                    $._account_component,
+                )
+            )
+        ),
+
+        _account_component: $ => /[A-Z0-9][a-zA-Z0-9-]*/,
+        _booking_method: $ => choice(
+            "STRICT",
+            "NONE",
+        ),
         date: $ => /\d\d\d\d[-/](0[1-9]|1[0-2])[-/]([012][0-9]|3[0-1])/,
         note: $ => /".*"/,
-        amount: $ => /\d+(.\d+)?/,
-        currency: $ => /[A-Z]+/,
-        comment: $ => /.?/,
+        amount: $ => /-?\d+(.\d+)?/,
+        commodity: $ => /[A-Z]+/,
+        comment: $ => seq(';', /.*/)
     }
 });
